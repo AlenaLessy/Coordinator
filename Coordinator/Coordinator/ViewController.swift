b80b4d18e05ecea8db67ababc9b174925c603aea
Алена Карпова <alenapancenko00@gmail.com>
2022-12-28 18:27:21 +0300

    init dev

--- Coordinator/Coordinator/ViewController.swift
+++ Coordinator/Coordinator/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
